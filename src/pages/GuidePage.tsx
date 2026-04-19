16bee1128ce737a7aa1ab3e12cc051d71a4e7ef1
lizhgb <lizhgb@yonyou.com>
2026-04-19 22:04:28 +0800

    docs: add export feature to guide page and progress tracking

--- src/pages/GuidePage.tsx
+++ src/pages/GuidePage.tsx
@@ -31,6 +31,7 @@ function GuidePage({ onNavigate }: GuidePageProps): JSX.Element {
               <li>{t('guide.feature8')}</li>
               <li>{t('guide.feature9')}</li>
               <li>{t('guide.feature10')}</li>
+              <li>{t('guide.feature11')}</li>
             </ul>
           </div>
         </div>
